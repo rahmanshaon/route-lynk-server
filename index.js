5f2138dde02ab822867912a05ba627ace0005d28
rahmanshaon <rahmanshaon45@gmail.com>
2025-12-08 06:14:53 +0600

    mongoDB connection added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Initialize the Express app
 const app = express();
@@ -10,12 +11,39 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 // Basic Route
 app.get("/", (req, res) => {
-  res.send("RouteLynk Server is Running");
+  res.send("RouteLynk Server is Running...");
 });
 
 // Start Server
 app.listen(port, () => {
-  console.log(`RouteLynk is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
